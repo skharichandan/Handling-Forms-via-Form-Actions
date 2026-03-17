7b63acc13b79a1cd8dbd9abd0405e83cd3ebbe6b
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-17 16:17:23 +0530

    Adding Validation Checks to Signup Form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,14 +1,49 @@
+import { isEmail, isNotEmpty, isEqualToOtherValue, hasMinLength } from "../util/validation.js";
+
 export default function Signup() {
 
   function signupAction (formData) {
     
-    const enteredEmail = formData.get('email');
-    const enteredPassword = formData.get('password');
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirm-password');
+    const firstName = formData.get('first-name');
+    const lastName = formData.get('last-name');
+    const role = formData.get('role');
+    const terms = formData.get('terms');
+    const acquisition = formData.getAll('acquisition');
 
-    console.log ({
-      enteredEmail,
-      enteredPassword,  
-    });
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push("Invalid email address");
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push("You must provide a password with at least 6 characters.");
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push("Passwords do not match.");
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("Please provide both your first and last name.");
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push("Please select your role.");
+    }
+
+    if (!terms) {
+      errors.push("You must agree to the terms and conditions.");
+    }
+
+    if (acquisition.length === 0) {
+      errors.push("Please select at least one acquisition channel.");
+    }
+
+    console.log(errors);
   }
   
   return (
